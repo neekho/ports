c96d3c63b4da435d6a674531813075f4426acdb5
Nico <remullanico28@gmail.com>
2024-02-02 23:39:25 +0800

    added skills/tools logos, description to be changed

--- home/models.py
+++ home/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+
+class Skills(models.Model):
+    slug = models.SlugField(unique=True, max_length=255, blank=True, null=True)
+
+    name = models.CharField(max_length=20)
+
+    description = models.TextField(max_length=20)
