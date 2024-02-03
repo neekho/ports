59eed122cf3b3c739aeee5fdd428c663e0231212
Nico <remullanico28@gmail.com>
2024-02-03 23:34:13 +0800

    hover on effects, changed footer, contact form update

--- home/models.py
+++ home/models.py
@@ -10,3 +10,23 @@ class Skills(models.Model):
     name = models.CharField(max_length=20)
 
     description = models.TextField(max_length=20)
+
+
+
+class Message(models.Model):
+
+    # subject, and name inclusion?
+
+    # SUBJECTS = {
+    #     "FR": "Freshman",
+    #     "SO": "Sophomore",
+    #     "JR": "Junior",
+    #     "SR": "Senior",
+    #     "GR": "Graduate",
+    # }
+
+    email = models.EmailField(blank=False, null=True)
+
+    # subject = models.CharField(max_length=28, choices=SUBJECTS, default='',)
+
+    message = models.TextField(max_length=255)
